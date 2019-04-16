4ee3cb0e58c05bcdd200f3e676bee0b8a6d3eebe
akshaygolash <akshay.golash95@gmail.com>
2019-04-15 21:34:44 -0500

    Initial commit with code setup --WIP

---
+++ AngularFeaturesApp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  public title = 'AngularFeaturesApp';
+  public features: any;
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.features = [{
+      title: 'Data Binding'
+    }];
+  }
+
+ngOnDestroy(): void {
+  
+  }
+
+}
---
+++ AngularFeaturesApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { MaterialModule } from './material-module';
+import { DataBindingComponent } from './features/data-binding/data-binding.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent
+    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
